91ee6ab9770dfaeddb0d150748f0c4a2a4503905
ScottiBR <guilhermescotti@gmail.com>
2021-08-17 00:44:17 -0300

    Change i18n client folder

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import '../node_modules/react-modal-video/css/modal-video.min.css';
 import 'react-accessible-accordion/dist/fancy-example.css';
 import '../public/css/style.css';
 import '../public/css/responsive.css';
-import './i18nClient';
+import '../utils/i18nClient';
 import { appWithTranslation } from 'next-i18next';
 
 import App from 'next/app';
